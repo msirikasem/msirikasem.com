d17ba43d7165d685ccf5f9acc7748000b4cffb74
jeremykerr <jeremy.p.kerr@gmail.com>
2021-02-20 22:36:09 -0800

    Initial commit, without portfolio.

---
+++ content/contact/_index.md
@@ -0,0 +1,8 @@
+---
+title            : "Contact"
+date             : 2021-02-20
+heading          : "Don’t be shy. Say <span>Hello.</span>"
+form_heading     : "TELL ME ABOUT YOUR GOALS"
+form_description : "Please send me an email at **msirikasem@gmail.com** with your name, a way to reach you, and a summary of what you'd like to collaborate on. I'll get back to you as soon as I can."
+---
+
---
+++ themes/roxo/assets/js/formhandler.js
@@ -0,0 +1,16 @@
+
+// helper function for sending an AJAX request
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
